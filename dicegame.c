254d9e25b319d39bd81500f5b59e7191f5de0673
s1250156 <40881890+s1250156@users.noreply.github.com>
2018-07-08 01:31:24 +0900

    Update dicegame.c

--- dicegame.c
+++ dicegame.c
@@ -5,21 +5,30 @@
 int main (){
 
   int d1,d2;
+  char st[1000];
+
+  printf("What is your name?\n");
+
+  printf("> ");
+
+  scanf("%s",st);
+
+  printf("Hello, %s!\n",st);
 
   srand(time(NULL));
 
   printf("Rolling the dice...\n");
-  
+
   d1=rand()%6+1;
   printf("Die 1:%d\n",d1);
-  
+
   d2=rand()%6+1;
   printf("Die 2:%d\n",d2);
 
   printf("Total value:%d\n",d1+d2);
-     
+
+  if((d1+d2)>=7)printf("%s won!\n",st);
+  else  printf("%s lost!\n",st);
   return 0;
 }
-
-
 	  
